e1136c777f1e4e13fc85f0601d17b0cb1a35a49b
Maksim.K <omega.kseno@gmail.com>
2023-02-27 01:28:19 +0300

    Oтформатировал код в файле  Program.cs

--- Work/Program.cs
+++ Work/Program.cs
@@ -12,68 +12,68 @@
 
 string[] CreatStringArray(int sizeArr)
 {
-string[] arr = new string[sizeArr];
-for(int i =0;i<sizeArr;i++)
-{
-  Console.Write($"Веедите строку {i+1} ->")  ;
-arr[i] = Console.ReadLine();
-}
-return arr;
+    string[] arr = new string[sizeArr];
+    for (int i = 0; i < sizeArr; i++)
+    {
+        Console.Write($"Веедите строку {i + 1} ->");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
 }
 
 //Метод для печати массива.
 
 void PrintStringArray(string[] arr)
 {
-Console.Write("[ ");
-for(int i =0;i<arr.Length;i++)
-{
-   if(i ==arr.Length-1)
-   { 
-   Console.Write(@$"""{arr[i]}""");
-   }
-   else
-   {
-    Console.Write(@$"""{arr[i]}"",");
-   }
-}
-Console.Write(" ]");
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == arr.Length - 1)
+        {
+            Console.Write(@$"""{arr[i]}""");
+        }
+        else
+        {
+            Console.Write(@$"""{arr[i]}"",");
+        }
+    }
+    Console.Write(" ]");
 
 }
 
 
 
 //Метод для подсчета количества строк меньше или равное 3 символам в массиве.
-int CountingCharArray(string[]arr)
+int CountingCharArray(string[] arr)
 {
-int count =0;
-for(int i =0;i<arr.Length;i++)
-{
-    if(arr[i].Length<=3) count++;
-}
-return  count;
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3) count++;
+    }
+    return count;
 }
 
 //Метод для формирования из массива строк в массив строк, из строк чье количество символов больше или равно 3.
-string[] CreatArrayShortString(String[]arr,int sizeResArr)
+string[] CreatArrayShortString(String[] arr, int sizeResArr)
 {
-int count = 0;
-string[] resArr = new string[sizeResArr];
-for(int i = 0;i<arr.Length;i++)
-{
-if(arr[i].Length<=3)
-{
-    resArr[count]=arr[i];
-    count++;
-}
-}
-return resArr;
+    int count = 0;
+    string[] resArr = new string[sizeResArr];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            resArr[count] = arr[i];
+            count++;
+        }
+    }
+    return resArr;
 }
 Console.Write("Введите какое количество строк вы хотите ввести  -> ");
 int size = Convert.ToInt32(Console.ReadLine());
 string[] array = CreatStringArray(size);
 PrintStringArray(array);
 int sizeResultArray = CountingCharArray(array);
-string[]ResultArray=CreatArrayShortString(array,sizeResultArray);
+string[] ResultArray = CreatArrayShortString(array, sizeResultArray);
 Console.Write("->");
 PrintStringArray(ResultArray);
